b52cee8aa9bca80393663de1ce24735dafbbfd22
ping zhang <pingzhang@pingdeMacBook-Pro.local>
2016-07-06 21:41:11 +0800

    添加materi风格的进度条

---
+++ MyApplication/app/src/main/java/com/example/zp/myapplication/fragment/MainFragment.java
@@ -0,0 +1,40 @@
+package com.example.zp.myapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by pingzhang on 16/7/6.
+ */
+public class MainFragment extends BaseFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
